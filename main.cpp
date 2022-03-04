596ebe3a7df74b496d8c1455d130d027df5e87fa
alah35 <maxnet663@gmail.com>
2022-03-04 14:40:33 +0300

    Ready for checking

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 #include "Tasks/Tasks.h"
 int main() {
-   task9();
+   int n = 0;
+   while (n != -1) {
+       std::cout << "For exit enter -1. Choose Task:";
+       std::cin >> n;
+       switch (n) {
+           case 1:
+               task1();
+               break;
+           case 2:
+               task2();
+               break;
+           case 3:
+               task3();
+               break;
+           case 4:
+               task4();
+               break;
+           case 5:
+               task5();
+               break;
+           case 6:
+               task6();
+               break;
+           case 7:
+               task7();
+               break;
+           case 8:
+               task8();
+               break;
+           case 9:
+               task9();
+               break;
+           case -1:
+               break;
+           default:
+               std::cout << "Wrong task number. Try again";
+               std::cin >> n;
+               break;
+
+       }
+   }
 }
